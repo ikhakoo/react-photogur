7e3402fc63545bf9307e2972d33c38f787262ddd
ikhakoo <ikhakoo@gmail.com>
2015-07-21 02:53:26 -0400

    creating pg db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150721054424) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pictures", force: :cascade do |t|
     t.string   "picurl"
     t.string   "pictitle"
